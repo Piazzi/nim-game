5047b904590bb95a2c779c3da2f929e6d036a763
Piazzi <40416044+Piazzi@users.noreply.github.com>
2022-01-11 13:10:50 -0300

    Add gameFinished / checkUserMove

--- app/Main.hs
+++ app/Main.hs
@@ -14,15 +14,25 @@ printBoard board = putStr $ unlines [replicate stars '|'| (stars, row) <- zip bo
 boardDivider :: IO()
 boardDivider = putStrLn "\n ---------- Board ------------ \n "
 
+-- Verify if the game have a winner checking wheter all elements in a list equals to 0
+-- meaning that every artifact were removed.
+gameFinished :: [Int] -> Bool
+gameFinished = all (==0)
+
+-- Verify whether a plater move is valid or not
+checkUserMove :: [Int] -> Int -> Int -> Bool
+checkUserMove board row artifacts = (board !! (row - 1)) >= artifacts
+
+
 main = do
        print "----- Starting the game -----"
        putStrLn "Select the dificulty: "
        putStrLn "[1] --> Easy Mode"
        putStrLn "[2] --> Hard Mode"
-       dificulty <- getLine  
+       dificulty <- getLine
        putStrLn "You selected: "
        print dificulty
-       putStrLn "Good Luck!"
+       putStrLn "The game is starting, good luck! \n"
        boardDivider
        printBoard board
        boardDivider
