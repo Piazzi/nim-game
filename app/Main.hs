3c87307eebb4656fc7919d91c3257cbe6dacc4b2
Piazzi <40416044+Piazzi@users.noreply.github.com>
2022-01-11 12:49:11 -0300

    Add board

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
-
+import Data.Char
 import Lib
+
+-- each index represents a line of the board, the value in each
+-- index represents the number of artifacts in the current board.
+board :: [Int]
+board = [1,3,5,7]
+
+-- print the current state of the board 
+printBoard :: [Int] -> IO ()
+printBoard board = putStr $ unlines [replicate stars '|'| (stars, row) <- zip board [1..length board]]
+
+boardDivider :: IO()
+boardDivider = putStrLn "\n ---------- Board ------------ \n "
 
 main = do
        print "----- Starting the game -----"
@@ -11,3 +23,7 @@ main = do
        putStrLn "You selected: "
        print dificulty
        putStrLn "Good Luck!"
+       boardDivider
+       printBoard board
+       boardDivider
+
