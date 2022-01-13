12f80c0f8fdb9e3c0de5966e2bf093a93d78bac3
Piazzi <40416044+Piazzi@users.noreply.github.com>
2022-01-13 18:33:01 -0300

    Update main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,49 @@
+-- Alunos: Lucas Piazzi de Castro (201635003) e Cristiano Nascimento (201635029) 
+
 {-# LANGUAGE BlockArguments #-}
 
 module Main where
-
 import Data.Char
 import Lib
+
+nimGame :: IO ()
+nimGame = do 
+  let dificulty = getDificulty 
+  let isPlayerGoingFirst = (if dificulty == 1 then 1 else 0)
+  main board isPlayerGoingFirst dificulty
+
+-- main function of the game
+main :: [Int] -> Int -> Int -> IO ()
+main board player dificulty =
+  do
+    boardDivider
+    printBoard board
+    boardDivider
+    if gameFinished board
+      then do
+        putStr "Player: "
+        putStr (show (nextTurn player))
+        putStrLn " wins!"
+      else do
+        putStr "Player "
+        print player
+        print " turn!"
+        -- player turn
+        if player == 1 then do
+        row <- getPlayerMove "Enter the row number you want to select: "
+        artifacts <- getPlayerMove "Enter the number of artifacts you want to remove: "
+        putStr "Move made!"
+        -- computer turn
+        else do
+          -- Head is the selected row to remove, and the tail is how many
+          -- artifacts will be removed
+          let rowAndArtifacts [] = getComputerMove board dificulty
+          main (updateBoard board (head rowAndArtifacts) (rowAndArtifacts !! 1) ) (nextTurn player) dificulty
+        if checkPlayerMove board row artifacts
+          then main (updateBoard board row artifacts) (nextTurn player) dificulty
+          else do
+            putStrLn "Invalid move, insert a valid move!"
+            main board player dificulty
 
 -- each index represents a line of the board, the value in each
 -- index represents the number of artifacts in the current board.
@@ -17,17 +57,8 @@ printBoard board = putStr $ unlines [replicate artifacts '|' | (artifacts, row) 
 boardDivider :: IO ()
 boardDivider = putStrLn "\n ---------- Board ------------ \n "
 
--- Verify if the game have a winner by checking wheter all elements in the board equals to 0
--- meaning that every artifact were removed.
-gameFinished :: [Int] -> Bool
-gameFinished = all (== 0)
-
--- Verify whether a plater move is valid or not
-checkPlayerMove :: [Int] -> Int -> Int -> Bool
-checkPlayerMove board row artifacts = board !! (row - 1) >= artifacts
-
 -- Update the board once a move is made by the player or computer
-updateBoard :: [Int] -> Int -> Int -> [Int]
+updateBoard :: [Int] -> Int -> Int -> [Int] -- returns a updated board
 updateBoard board row rowArtifacts = [if rowIndex == row then artifacts - rowArtifacts else artifacts | (artifacts, rowIndex) <- zip board [1 .. length board]]
 
 -- Get the player move via input
@@ -41,10 +72,30 @@ getPlayerMove move = do
       putStrLn "Invalid input, insert a valid number!"
       getPlayerMove move
 
+-- Get the computer move based on the difficulty selected
+getComputerMove :: [Int] -> [Int]
+getComputerMove dificulty = do
+  if dificulty == 1 then makeRandomMove board else makeOptimalMove board
+
+-- Return a random move for the easy mode
+makeRandomMove ::[Int] -> [Int]
+
+-- Return optimal move for the hard mode
+makeOptimalMove ::[Int] -> [Int]
+
+-- Verify whether a plater move is valid or not
+checkPlayerMove :: [Int] -> Int -> Int -> Bool
+checkPlayerMove board row artifacts = board !! (row - 1) >= artifacts
+
+-- Verify if the game have a winner by checking wheter all elements in the board equals to 0
+-- meaning that every artifact were removed.
+gameFinished :: [Int] -> Bool
+gameFinished = all (== 0)
+
 -- check player dificulty
-getDificulty :: String -> IO Int
-getDificulty dificulty = do
-  putStr dificulty
+getDificulty :: IO Int
+getDificulty  = do
+  putStrLn "----- Starting the game -----"
   putStrLn "Select the dificulty: "
   putStrLn "[1] --> Easy Mode"
   putStrLn "[2] --> Hard Mode"
@@ -57,39 +108,11 @@ getDificulty dificulty = do
       return (digitToInt playerInput)
     else do
       putStrLn "Invalid dificulty, insert a valid dificulty: \n [1] - For Easy Mode \n [2] - For Hard Mode "
-      getDificulty dificulty
+      getDificulty
 
 -- Get wheter is the player turn or the computer
--- player = 0--> computer turn
+-- player = 0 --> computer turn
 -- player = 1 --> player turn
 nextTurn :: Int -> Int
-nextTurn player = if player == 0 then 1 else 0 
+nextTurn player = if player == 0 then 1 else 0
 
-
--- main function of the game
-main :: [Int] -> Int -> IO ()
-main board player = 
- do
-  getDificulty "----- Starting the game -----"
-  do
-    boardDivider
-    printBoard board
-    boardDivider
-    if gameFinished board
-      then do
-        putStr "Player "
-        putStr (show (nextTurn player))
-        putStrLn " wins!"
-      else do
-        putStr "Player "
-        print player
-        row <- getPlayerMove "Enter the row number you want to select: "
-        artifacts <- getPlayerMove "Enter the number of artifacts you want to remove: "
-        if checkPlayerMove board row artifacts
-          then main (updateBoard board row artifacts) (nextTurn player)
-          else do
-            putStrLn "Invalid move, insert a valid move!"
-            main board player
-
-nim :: IO ()
-nim = main board 1
